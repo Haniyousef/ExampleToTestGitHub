930824612df9a28149ad8149bfa8ee974bb36e2f
haniyousef <haniyousef49@gmail.com>
2017-10-29 22:05:44 +0200

    Modifyed  MainActivity and Added TestAdd

--- app/src/main/java/com/example/hani/exampletotestgithub/MainActivity.java
+++ app/src/main/java/com/example/hani/exampletotestgithub/MainActivity.java
@@ -14,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void testUpload(){
         int i=1+1 ;
+
+        // make my first change in this file
+        // notic >>>> the color of file name mean that there is change in file not add to github
+        int x=2+2;
     }
 }
---
+++ app/src/main/java/com/example/hani/exampletotestgithub/TestAdd.java
@@ -0,0 +1,11 @@
+package com.example.hani.exampletotestgithub;
+
+/**
+ * Created by Hani on 10/29/2017.
+ */
+
+public class TestAdd {
+
+    // Test add new created file to the project on github
+    // notic >>>> the color of file name mean that it not added to project yet
+}
